0d88667018f66adcf7316380450433f2a2ad091f
BaoLanNguyen <bang03001@utb.vaxjo.se>
2023-01-18 10:09:40 +0100

    Add project files.

---
+++ Uppgift 1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uppgift_1._2
+{
+
+    class program2
+    {
+        static void Main(string[] args)
+        {
+            // Ändrar bakgrunds färg.
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            Console.WriteLine("Kunde inte hitta 'system.32'. Kontakta 'microsoft support' för mer information.");
+            
+
+        }
+
+    }
+
+}
